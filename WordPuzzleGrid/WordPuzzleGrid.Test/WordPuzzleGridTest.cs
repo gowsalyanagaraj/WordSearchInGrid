965a1ad015526e7d2e851cf07d89c8252a39b46a
Gowsalya Nagaraj <gowsalyanagaraj07@gmail.com>
2025-07-29 11:28:30 +0530

    MODIFIED: Test Cases

--- WordPuzzleGrid/WordPuzzleGrid.Test/WordPuzzleGridTest.cs
+++ WordPuzzleGrid/WordPuzzleGrid.Test/WordPuzzleGridTest.cs
@@ -15,6 +15,27 @@
 				 ['A','D','E','E']
 				};
 
+			solution.PuzzleGrid.SearchWord = "ABCCED";
+
+			// Act
+			bool result = solution.SearchWordInGrid();
+
+			//Assert
+			Assert.AreEqual(result, true);
+		}
+		
+		[TestMethod]
+		public void TestMethod2()
+		{
+			// Arrange
+			Solution solution = new Solution();
+
+			solution.PuzzleGrid.Board =  new char[][]{
+				 ['A','B','C','E'],
+				 ['S','F','C','S'],
+				 ['A','D','E','E']
+				};
+
 			solution.PuzzleGrid.SearchWord = "FAC";
 
 			// Act
